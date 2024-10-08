b690b96cdda753aadc2288ab600d0cd9940d49fc
Max Boldyrev <bm456@ya.ru>
2024-10-09 02:17:16 +0300

    Working on v3d

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ class GeoTest(unittest.TestCase):
 
     def testpvsum(self):
         p = geo.Point(5, 7)
-        v = geo.Vecor(2, 3)
+        v = geo.Vector(2, 3)
         self.assertEqual(v + p, geo.Point(7, 10))
 
     def testlen(self):
-        v = geo.Vecor(3, 3)
+        v = geo.Vector(3, 3)
         self.assertEqual(v.len, (3**2 + 3**2) ** 0.5)
 
 
